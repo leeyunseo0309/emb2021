a2a351ced5b718929b1e25225017fedae27a6fc3
wkdtjdghks <sunghwan97@naver.com>
2021-11-14 19:58:38 +0900

    조원 과제 작성함
    1. 1.c, 2.c 추가함
    2. myProject.h 만듦

---
+++ 1.c
@@ -0,0 +1,6 @@
+#include "myProject.h"
+int printMyName1(void)
+{
+	printf("1. my name is [장성환]\n");
+	return 1;
+}
---
+++ 2.c
@@ -0,0 +1,6 @@
+#include "myProject.h"
+int printMyName2(void)
+{
+	printf("2. my name is [장성환]\n");
+	return 2;
+}
